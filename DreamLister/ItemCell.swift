3f48db6db77c8e96f0725d614f073c55ef672952
Burak <burakfirik@gmail.com>
2017-02-14 13:14:31 -0800

    Fetching and displaying data

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Burak Firik on 2/13/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Burak Firik on 2/13/17.
+//  Copyright © 2017 Code Path. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
